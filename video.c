2618b901429f0170b317af18354a43de0c9b328e
bakape <scorpid33@gmail.com>
2017-10-20 15:18:52 +0300

    tmp

--- video.c
+++ video.c
@@ -1,44 +1,88 @@
 #include "video.h"
+#include <libavfilter/avfilter.h>
 #include <libavutil/imgutils.h>
 #include <libswscale/swscale.h>
 
-const size_t frameBufferSize = 1 << 17;
+/**
+ * Potential thumbnail lookup filter to reduce the risk of an inappropriate
+ * selection (such as a black frame) we could get with an absolute seek.
+ *
+ * Simplified version of algorithm by Vadim Zaliva <lord@crocodile.org>.
+ * http://notbrainsurgery.livejournal.com/29773.html
+ *
+ * Adapted by Janis Petersons <bakape@gmail.com>
+ */
 
-int extract_video_image(struct Buffer* img, AVFormatContext* avfc,
-    AVCodecContext* avcc, const int stream)
+#define HIST_SIZE (3 * 256)
+#define MAX_FRAMES 100
+
+// Compute sum-square deviation to estimate "closeness"
+static double compute_error(
+    const int hist[HIST_SIZE], const double median[HIST_SIZE])
 {
-    int err;
-    AVPacket pkt;
-    AVFrame* frame = av_frame_alloc();
+    double sum_sq_err = 0;
+    for (int i = 0; i < HIST_SIZE; i++) {
+        const double err = median[i] - (double)hist[i];
+        sum_sq_err += err * err;
+    }
+    return sum_sq_err;
+}
 
-    for (;;) {
-        err = av_read_frame(avfc, &pkt);
-        if (err < 0) {
+// RGB color distribution histograms of the frames. Reused between all calls to
+// avoid allocations. Need lock of hist_mu.
+static int hists[MAX_FRAMES][HIST_SIZE];
+pthread_mutex_t hist_mu = PTHREAD_MUTEX_INITIALIZER;
+
+// Select best frame based on RGB histograms
+static int select_best_frame(AVFrame* frames[], int* best_i)
+{
+    int err = pthread_mutex_lock(&hist_mu);
+    if (err) {
+        return err;
+    }
+
+    // First rezero after last use
+    memset(hists, 0, sizeof(int) * MAX_FRAMES * HIST_SIZE);
+
+    // Compute each frame's histograms
+    int frame_i;
+    for (frame_i = 0; frame_i < MAX_FRAMES; frame_i++) {
+        const AVFrame* f = frames[frame_i];
+        uint8_t* p = f->data[0];
+        if (!f) {
+            frame_i--;
             break;
         }
-
-        if (pkt.stream_index == stream) {
-            err = avcodec_send_packet(avcc, &pkt);
-            if (err < 0) {
-                break;
+        for (int j = 0; j < f->height; j++) {
+            for (int i = 0; f->width; i++) {
+                for (int k = 0; k < 3; k++) {
+                    hists[frame_i][k * 256 + p[i * 3 + k]]++;
+                }
             }
-
-            err = avcodec_receive_frame(avcc, frame);
-            if (err == AVERROR(EAGAIN)) {
-                av_packet_unref(&pkt);
-                continue;
-            } else if (err < 0) {
-                break;
-            }
-
-            err = encode_frame(img, frame);
-            break;
+            p += f->linesize[0];
         }
     }
 
-    av_packet_unref(&pkt);
-    av_frame_free(&frame);
-    return err;
+    // Average histograms of up to 100 frames
+    double average[HIST_SIZE] = { 0 };
+    for (int j = 0; j <= frame_i; j++) {
+        for (int i = 0; i <= frame_i; i++) {
+            average[j] = (double)hists[i][j];
+        }
+        average[j] /= frame_i + 1;
+    }
+
+    // Find the frame closer to the average using the sum of squared errors
+    double min_sq_err = -1;
+    for (int i = 0; i <= frame_i; i++) {
+        const double sq_err = compute_error(hists[i], average);
+        if (i == 0 || sq_err < min_sq_err) {
+            *best_i = i;
+            min_sq_err = sq_err;
+        }
+    }
+
+    return pthread_mutex_unlock(&hist_mu);
 }
 
 // Encode frame to RGBA image
@@ -67,4 +111,67 @@ static int encode_frame(struct Buffer* img, const AVFrame const* frame)
 
     sws_freeContext(ctx);
     return 0;
+}
+
+int extract_video_image(struct Buffer* img, AVFormatContext* avfc,
+    AVCodecContext* avcc, const int stream)
+{
+    int err = 0;
+    int best_frame = 0;
+    int frame_i = 0;
+    AVPacket pkt;
+    AVFrame* frames[MAX_FRAMES] = { 0 };
+
+    // Read up to 100 frames
+    while (1) {
+        err = av_read_frame(avfc, &pkt);
+        if (err < 0) {
+            goto end;
+        }
+
+        if (pkt.stream_index == stream) {
+            err = avcodec_send_packet(avcc, &pkt);
+            if (err < 0) {
+                goto end;
+            }
+
+            if (!frames[frame_i]) {
+                frames[frame_i] = av_frame_alloc();
+            }
+            err = avcodec_receive_frame(avcc, frames[frame_i]);
+            switch (err) {
+            case 0:
+                if (++frame_i == MAX_FRAMES) {
+                    goto end;
+                }
+                av_packet_unref(&pkt);
+                continue;
+            case AVERROR(EAGAIN):
+                av_packet_unref(&pkt);
+                continue;
+            default:
+                goto end;
+            }
+        }
+    }
+
+end:
+    switch (err) {
+    case AVERROR_EOF:
+        err = 0;
+    case 0:
+        err = select_best_frame(frames, &best_frame);
+        if (!err) {
+            err = encode_frame(img, frames[best_frame]);
+        }
+        break;
+    }
+    av_packet_unref(&pkt);
+    for (int i = 0; i < MAX_FRAMES; i++) {
+        if (!frames[i]) {
+            break;
+        }
+        av_frame_free(&frames[i]);
+    }
+    return err;
 }
--- video.h
+++ video.h
@@ -4,5 +4,3 @@
 
 int extract_video_image(struct Buffer* img, AVFormatContext* avfc,
     AVCodecContext* avcc, const int stream);
-
-static int encode_frame(struct Buffer* img, const AVFrame const* frame);

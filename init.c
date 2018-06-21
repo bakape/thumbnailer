825667d4b39891c13587ffefc12c16a3f19d052c
bakape <scorpid33@gmail.com>
2018-06-21 07:47:32 +0300

    Revert "Revert "Do not patch GM signal handlers""
    This reverts commit f18ca05c6bdbcbdc2b7c509576486da1d0acb4aa.
    
    False positive

--- init.c
+++ init.c
@@ -1,51 +1,6 @@
 #include "init.h"
 #include <magick/api.h>
 #include <signal.h>
-#include <stdio.h>
-#include <string.h>
-
-void magickInit()
-{
-    InitializeMagick(NULL);
-
-#ifndef _WIN32
-
-#if defined(SIGCHLD)
-    fixSignal(SIGCHLD);
-#endif
-#if defined(SIGHUP)
-    fixSignal(SIGHUP);
-#endif
-#if defined(SIGINT)
-    fixSignal(SIGINT);
-#endif
-#if defined(SIGQUIT)
-    fixSignal(SIGQUIT);
-#endif
-#if defined(SIGABRT)
-    fixSignal(SIGABRT);
-#endif
-#if defined(SIGFPE)
-    fixSignal(SIGFPE);
-#endif
-#if defined(SIGTERM)
-    fixSignal(SIGTERM);
-#endif
-#if defined(SIGBUS)
-    fixSignal(SIGBUS);
-#endif
-#if defined(SIGSEGV)
-    fixSignal(SIGSEGV);
-#endif
-#if defined(SIGXCPU)
-    fixSignal(SIGXCPU);
-#endif
-#if defined(SIGXFSZ)
-    fixSignal(SIGXFSZ);
-#endif
-
-#endif
-}
 
 #ifndef _WIN32
 // Add the SA_ONSTACK flag to a listened on signal to play nice with the Go
@@ -53,12 +8,26 @@ void magickInit()
 static void fixSignal(int signum)
 {
     struct sigaction st;
-
     if (sigaction(signum, NULL, &st) < 0) {
         return;
     }
-
-    st.sa_flags |= SA_ONSTACK;
+    st.sa_flags |= 0x08000000;
     sigaction(signum, &st, NULL);
 }
 #endif
+
+void magickInit()
+{
+    InitializeMagick(NULL);
+
+#ifndef _WIN32
+
+#if defined(SIGINT)
+    fixSignal(SIGINT);
+#endif
+#if defined(SIGTERM)
+    fixSignal(SIGTERM);
+#endif
+
+#endif
+}
--- init.h
+++ init.h
@@ -2,10 +2,5 @@
 #define CGO_THUMBNAILER_INIT_H
 
 void magickInit();
-static void fixSignal(int signum);
-
-#ifndef SA_ONSTACK
-#define SA_ONSTACK 0x08000000
-#endif
 
 #endif
